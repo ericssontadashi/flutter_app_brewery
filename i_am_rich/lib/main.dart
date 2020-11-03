295918b8b6f4f51213537bccd4c7d86831a257e3
Ericsson Oliveira <cssonddos@gmail.com>
2020-11-03 07:48:37 -0300

    Scaffold lesson

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -3,9 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World1'),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('I am rich'),
+        backgroundColor: Colors.blueGrey[900],
       ),
-    ),
+      backgroundColor: Colors.blue[400],
+      body: Center(
+        child: Image(
+          image: NetworkImage('https://www.w3schools.com/w3css/img_lights.jpg'),
+        ),
+      ),
+    )),
   );
 }
