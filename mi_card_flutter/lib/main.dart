be4df62853c6facb8b9f0a4cf9838dfb10e10f1f
Ericsson Oliveira <cssonddos@gmail.com>
2020-11-04 09:36:21 -0300

    Fonts, Column and Row lesson

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -9,8 +9,41 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black,
-        body: Container(),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[Container()],
+            ),
+            CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.red,
+              backgroundImage: AssetImage('images/avatar.jpeg'),
+            ),
+            Text(
+              'Ericsson de Oliveira',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 40,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 20,
+                  color: Colors.teal.shade100,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5),
+            )
+          ],
+        )),
       ),
     );
   }
