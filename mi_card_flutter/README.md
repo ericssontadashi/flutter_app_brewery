0c4499a0dc9fcc3ea4795796c35e1ffeeae0bb50
Ericsson Oliveira <cssonddos@gmail.com>
2020-11-05 06:36:31 -0300

    Material icons lesson

--- mi_card_flutter/README.md
+++ mi_card_flutter/README.md
@@ -26,3 +26,6 @@ Mi Card is a personal business card. Imagine every time you wanted to give someo
 >This is a companion project to The App Brewery's Complete Flutter Development Bootcamp, check out the full course at [www.appbrewery.co](https://www.appbrewery.co/)
 
 ![End Banner](https://github.com/londonappbrewery/Images/blob/master/readme-end-banner.png)
+
+https://www.materialpalette.com/
+https://material.io/resources/icons/?style=baseline
--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -41,6 +41,49 @@ class MyApp extends StatelessWidget {
                   color: Colors.teal.shade100,
                   fontWeight: FontWeight.bold,
                   letterSpacing: 2.5),
+            ),
+            Container(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              padding: EdgeInsets.all(10),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    '+55 11 99999-9999',
+                    style: TextStyle(fontFamily: 'SourceSansPro', fontSize: 20),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    'cssonddos@gmail.com',
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSansPro'),
+                  ),
+                ],
+              ),
             )
           ],
         )),
