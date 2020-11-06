b353563db735b5979fc8cf26c6fc23f5b22d8a04
Ericsson Oliveira <cssonddos@gmail.com>
2020-11-06 08:01:05 -0300

    Magic ball lesson

---
+++ magic-8-ball-flutter/build/app/generated/source/buildConfig/debug/co/appbrewery/magic8ball/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package co.appbrewery.magic8ball;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "co.appbrewery.magic8ball";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
